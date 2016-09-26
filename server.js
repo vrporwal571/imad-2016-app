ba101b969650ddac32c6526f2b0ddb4d4fc91798
vrporwal571 <vrporwal571@gmail.com>
2016-09-26 11:04:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one is requested and will be served');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two is requested and will be served');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article three is requested and will be served');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
